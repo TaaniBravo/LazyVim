e36fcd4fcc2566d94bdd62dfe158018a05a0bca2
Taanileka Maama <tstmaama@sbgtv.com>
2023-06-03 18:03:30 -0700

    Check dap.adapters before adding python

--- lua/plugins/dap/python.lua
+++ lua/plugins/dap/python.lua
@@ -1,15 +1,18 @@
 local M = {}
 
 function M.setup()
+  local dap = require("dap")
   require("dap-python").setup("python", {})
 
-  require("dap").adapters.python = {
-    type = "executable",
-    command = os.getenv("HOME") .. "/.virtualenvs/debugpy/bin/python",
-    args = { "-m", "debugpy.adapter" },
-  }
+  if not dap.adapters.python then
+    dap.adapters.python = {
+      type = "executable",
+      command = os.getenv("HOME") .. "/.virtualenvs/debugpy/bin/python",
+      args = { "-m", "debugpy.adapter" },
+    }
+  end
 
-  require("dap").configurations.python = {
+  dap.configurations.python = {
     {
       name = "Python: Current File",
       type = "python",
